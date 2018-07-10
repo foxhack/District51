8889427f3857f3c7720ba2770bb3f7cec90f1bb9
Liang Guo <liang_guo@hansight.com>
2018-07-10 18:16:45 +0800

    Initialize the ice2 project.

---
+++ ice2/src/main/java/com/hansight/engine/ice2/ice2/Ice2Application.java
@@ -0,0 +1,12 @@
+package com.hansight.engine.ice2.ice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ice2Application.class, args);
+	}
+}
